e5beaf4f8034c844b494737e5c95cf0799ce46eb
Kotilainen Ilkka Antti <ilkka.a.kotilainen@gmail.com>
2021-10-23 21:17:39 +0300

    2nd

---
+++ postman.js
@@ -0,0 +1,3 @@
+var parts = ["antiikdev", "gmail", "com", "&#46;", "&#64;"];
+var writeEmail = parts[0] + parts[4] + parts[1] + parts[3] + parts[2];
+document.getElementById("email").innerHTML=writeEmail;
